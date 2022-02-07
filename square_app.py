7b3c6dc22dd1edc29e7770404f04ef4da6c66352
M0darwish <mdarwishinbox@gmail.com>
2022-02-07 16:40:26 +0000

    flask day one

---
+++ square_app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+
+@app.route('/home')
+def home():
+    return 'This is the home page'
+
+@app.route('/home/<int:number>')
+def homesq(number):
+
+    return  str(number ** 2)
+
+
+@app.route('/about')
+def about():
+    return 'This is the about page'
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
