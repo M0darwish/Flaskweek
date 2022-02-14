1193453fed072d0e5494e74933a8d294d1999c61
M0darwish <mdarwishinbox@gmail.com>
2022-02-14 16:05:39 +0000

    todo day one - my draft

--- todo_project/todo_day_one/application/routes.py
+++ todo_project/todo_day_one/application/routes.py
@@ -1,22 +1,22 @@
 from application import app, db
 from application.models import Todo
 
-@app.route('/add/<task>')
+@app.route('/add')
 def add():
     new_task = Todo(task="New test task")
     db.session.add(new_task)
     db.session.commit()
     return "Added new test task to the database"
 
-# @app.route('/update/<task>')
-# def update(task):
-#     first_task = Todo.query.first()
-#     first_task.task = task
-#     db.session.commit()
+@app.route('/update/<task>')
+def update(task):
+    first_task = Todo.query.first()
+    first_task.task = task
+    db.session.commit()
 
-#     return (f"Your task : {first_task.task}. is added to the database")
+    return (f"Your task : {first_task.task}. is added to the database")
 
-# @app.route('/')
+@app.route('/')
 @app.route('/view')
 def read():
     all_tasks = Todo.query.all()
@@ -30,4 +30,4 @@ def delete():
     first_task = Todo.query.first()
     db.session.delete(first_task)
     db.session.commit()
-    return "You have deleted the first Task on the"
+    return "You have deleted the first Task on the Todo list"
